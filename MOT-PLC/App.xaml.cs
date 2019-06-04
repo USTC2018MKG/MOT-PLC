6728682d19746b4c4edce7e25062a262e87c3fa5
Mht <hitomeng@outlook.com>
2019-06-04 15:37:56 +0800

    add readme.md

--- MOT-PLC/App.xaml.cs
+++ MOT-PLC/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,29 @@ namespace MOT_PLC
     /// </summary>
     public partial class App : Application
     {
+       
+
+        private void QueryOrder()
+        {
+            // 查询订单状态
+            // 0 未操作
+            // 1 需要PLC出货
+
+            // 
+            while (true)
+            {
+                
+                Thread.Sleep(1000);
+
+            }
+        }
+
+       
+        private void Application_Activated(object sender, EventArgs e)
+        {
+            ThreadStart childref = new ThreadStart(QueryOrder);
+            Thread query = new Thread(childref);
+            query.Start();
+        }
     }
 }
